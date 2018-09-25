631afd9daf9e9e320de76784993ffa55feef9a35
LaYt <laytya@gmail.com>
2018-09-25 09:34:17 +0700

    Add ruRU locale

---
+++ _LazySpell/locals_ruRU.lua
@@ -0,0 +1,24 @@
+
+
+local L = AceLibrary("AceLocale-2.0"):new("LazySpell")
+
+L:RegisterTranslations("ruRU", function() return {
+
+	["Options"] = "Настройки",
+	["Open options"] = "Открыть настройки",
+	["Max spell ranks"] = "Максимальные ранги заклинаний",
+	["Options for max rank spell to subtitude."] = "Настройки максимальных рангов заклинаний для подмены",
+	["Healing Wave Max Rank"] = "Волна исцеления",
+	["Defines the max rank of Healing Wave spell used by lazyspell."] = "Устанавливает максимальный ранг заклинания Волна исцеления, используемый lazyspell",
+	["Lesser Healing Wave Max Rank"] = "Малая волна исцеления",
+	["Defines the max rank of Lesser Healing Wave spell used by lazyspell."] = "Устанавливает максимальный ранг заклинания Малая волна исцеления, используемый lazyspell",
+	["Chain Heal Max Rank"] = "Цепное исцеление",
+	["Defines the max rank of Chain Heal spell used by lazyspell."] = "Устанавливает максимальный ранг заклинания Цепное исцеление, используемый lazyspell",
+	["Heal Max Rank"] = "Исцеление",
+	["Defines the max rank of Heal spell used by lazyspell."] = "Устанавливает максимальный ранг заклинания Исцеление, используемый lazyspell",
+	["Flash Heal Max Rank"] = "Быстрое исцеление",
+	["Defines the max rank of Flash Heal spell used by lazyspell."] = "Устанавливает максимальный ранг заклинания Быстрое исцеление, используемый lazyspell",
+	["Greater Heal Max Rank"] = "Великое исцеление",
+	["Defines the max rank of Greater Heal spell used by lazyspell."] = "Устанавливает максимальный ранг заклинания Великое исцеление, используемый lazyspell",
+
+} end)
